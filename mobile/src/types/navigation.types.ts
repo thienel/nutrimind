ba71a11da39cf63bc70bdf276ea4b6f708b890e0
longnhat03072005 <npln.0307@gmail.com>
2026-06-02 20:01:03 +0700

    feat: navigation stack

--- mobile/src/types/navigation.types.ts
+++ mobile/src/types/navigation.types.ts
@@ -1,11 +1,48 @@
+import type { NavigatorScreenParams } from '@react-navigation/native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-export type RootStackParamList = {
+export type AuthStackParamList = {
   SignIn: undefined;
-  Onboarding: undefined;
-  MainTabs: undefined;
 };
 
-export type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
+export type HomeStackParamList = {
+  Home: undefined;
+  WaterTracking: undefined;
+};
+
+export type MealStackParamList = {
+  MealLog: undefined;
+  ManualFoodEntry: undefined;
+  PhotoAnalysis: undefined;
+};
+
+export type AICoachStackParamList = {
+  AICoach: undefined;
+};
+
+export type ProgressStackParamList = {
+  Progress: undefined;
+};
+
+export type ProfileStackParamList = {
+  Profile: undefined;
+  Reminders: undefined;
+};
+
+export type MainTabParamList = {
+  HomeStack: NavigatorScreenParams<HomeStackParamList>;
+  MealStack: NavigatorScreenParams<MealStackParamList>;
+  AICoachStack: NavigatorScreenParams<AICoachStackParamList>;
+  ProgressStack: NavigatorScreenParams<ProgressStackParamList>;
+  ProfileStack: NavigatorScreenParams<ProfileStackParamList>;
+};
+
+export type RootStackParamList = {
+  Auth: NavigatorScreenParams<AuthStackParamList>;
+  Onboarding: undefined;
+  MainTabs: NavigatorScreenParams<MainTabParamList>;
+};
+
+export type SignInScreenProps = NativeStackScreenProps<AuthStackParamList, 'SignIn'>;
 export type OnboardingScreenProps = NativeStackScreenProps<RootStackParamList, 'Onboarding'>;
 export type MainTabsScreenProps = NativeStackScreenProps<RootStackParamList, 'MainTabs'>;
