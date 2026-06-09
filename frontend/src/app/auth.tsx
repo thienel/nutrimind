4fad488dffd87989155af2647e374bdd6754d18a
linh20051708 <dieulinh20051708@gmail.com>
2026-06-09 10:05:17 +0700

    Convert frontend to Expo Router and add auth flow

---
+++ frontend/src/app/auth.tsx
@@ -0,0 +1,5 @@
+import { Auth } from "@/pages/Auth";
+
+export default function AuthScreen() {
+  return <Auth />;
+}
---
+++ frontend/src/app/first-week-plan.tsx
@@ -0,0 +1,5 @@
+import { FirstWeekPlan } from "@/pages/FirstWeekPlan";
+
+export default function FirstWeekPlanScreen() {
+  return <FirstWeekPlan />;
+}
